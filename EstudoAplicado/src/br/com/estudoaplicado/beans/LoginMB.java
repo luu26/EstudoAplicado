5e1032ee1f8f48b2f403d8e0262a02759370ae3f
lumartins <lucas.oliveira2609@gmail.com>
2016-08-02 11:38:56 -0300

    Aula 5a -OK

--- EstudoAplicado/src/br/com/estudoaplicado/beans/LoginMB.java
+++ EstudoAplicado/src/br/com/estudoaplicado/beans/LoginMB.java
@@ -3,31 +3,39 @@ package br.com.estudoaplicado.beans;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean
-public class HomeMB implements Serializable{
+@RequestScoped
+public class LoginMB implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String nome;
+	private String login;
+	private String senha;
 	
-	public HomeMB() {
+	public LoginMB() {
 		System.out.println("Criando instância do HomeMB");
 	}
 	
 	public void executar(){
 		System.out.println("Início do método executar");
-		System.out.println("Nome do usuário : "+nome);
+		System.out.println("Nome do usuário : "+login);
 	}
 
 	public String getNome() {
-		return nome;
+		return login;
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
+		this.login = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 }
