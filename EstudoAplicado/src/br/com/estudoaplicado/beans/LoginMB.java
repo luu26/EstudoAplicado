c3d56b43b70ea4172e8632bda5026e00bdc2968d
lumartins <lucas.oliveira2609@gmail.com>
2016-08-10 16:41:32 -0300

    Alteração de login

--- EstudoAplicado/src/br/com/estudoaplicado/beans/LoginMB.java
+++ EstudoAplicado/src/br/com/estudoaplicado/beans/LoginMB.java
@@ -7,27 +7,33 @@ import javax.faces.bean.RequestScoped;
 
 @ManagedBean
 @RequestScoped
-public class LoginMB implements Serializable{
+public class LoginMB implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	
+	private static final long serialVersionUID = 987387080925261041L;
 	private String login;
 	private String senha;
-	
+
 	public LoginMB() {
-		System.out.println("Criando instância do HomeMB");
-	}
-	
-	public void executar(){
-		System.out.println("Início do método executar");
-		System.out.println("Nome do usuário : "+login);
+		System.out.println("Criando instância do LoginMB");
+		
 	}
 
-	public String getNome() {
+	String exibirPaginaLogin() {
+		return "login";
+	}
+
+	public String logar() {
+		System.out.println("Entrou no método Logar!");
+			return "/pages/home";
+	}
+
+	public String getLogin() {
 		return login;
 	}
 
-	public void setNome(String nome) {
-		this.login = nome;
+	public void setLogin(String login) {
+		this.login = login;
 	}
 
 	public String getSenha() {
